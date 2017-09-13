baf7bb3b961ea25e41d1d3500013138167bdb640
xiaoyuann <602632139@qq.com>
2017-09-13 22:05:50 +0800

    E

---
+++ python basic/hello.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''s4=input()
+s5=input()
+s1=int(s4)
+s2=int(s5)
+s3=((s2-s1)/s1)*100
+print('小明的成绩提高了%.2f%%' % s3)
+novel=["七界传说","斗破苍穹"]
+print(novel[1])
+novel.insert(1,'大主宰')
+print(novel[2])
+novel.pop(1)
+print(novel)
+print(len(novel))
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0])
+sum=0
+for s in range(101):
+	sum=sum+s
+	print(sum)
+print(sum)
+L = ['Bart', 'Lisa', 'Adam']
+for h in L:
+	print('hello,%s' % h)
+do={'bob':12,'lucy':15,'tom':16}
+print(do['bob'])
+d=do.get('bob',-1)
+print(d)'''
+s=set([1,[2,3],3])
+print(s)
