2fe818571f42f5966fcf9ea1c2c91c8a347b44cb
xiaoyuann <602632139@qq.com>
2017-09-14 23:18:17 +0800

    1

--- python basic/hello.py
+++ python basic/hello.py
@@ -58,7 +58,7 @@ print(append())
 
 def person(name,phone,**kw):
     print('name:',name,'phone:','other:',kw)
-person('bob',12586,number='3568+')'''
+person('bob',12586,number='3568+')
 
 
 
@@ -69,3 +69,67 @@ def fact_iter(num, product):
 def fact(n):
     return fact_iter(n, 1)
 print(fact(5))
+
+def hanoi(n,A,B,C):
+    if n==1:
+        print(A,"--->",C)
+    else:
+        hanoi(n-1,A,C,B)
+        hanoi(1,A,B,C)
+        hanoi(n-1,B,A,C)
+
+hanoi(10,'A','B','C')
+
+from collections import Iterable
+print(isinstance('ishjdsvn',Iterable))
+for k in 'kfnskjnf':
+    print(k)
+
+l=[1,2,4,54]
+print(l[-2:-1])
+
+l=[x*x for x in range(1,10001) if x%2 == 0]
+print(l)
+
+print([m + n for m in 'ABC' for n in 'XYZ'])
+
+import os
+print([d for d in os.listdir('.')])
+
+def tri():
+    b=[1]
+    while True:
+        yield b
+        b=[1]+[sum(b[i:i+2]) for i in range(len(b)-1)]+[1]
+
+n=0
+for t in tri():
+  print(t)
+  n+=1
+  if n>10:
+     break
+
+L1=['adam','LISA','barT']
+def normalize(name):
+   return name[0].upper()+name[1:].lower()
+print(list(map(normalize,L1)))
+
+l=[1,2,5,98,6]
+print(l[::2])
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+    return t[0]
+
+L2 = sorted(L, key=by_name)
+print(L2)
+
+def by_score(t):
+    return t[1]
+
+L2 = sorted(L, key=by_score,reverse=True)
+print(L2)'''
+
+from PIL import Image
+im = Image.open('test.png')
+print(im.format, im.size, im.mode)
