79bc2c5817607101b6c28e26659cff4e6fa8a6ad
xiaoyuann <602632139@qq.com>
2017-09-18 18:58:31 +0800

    1

--- webapp/www/app.py
+++ webapp/www/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__author__ = 'dawn'
+
+
 import logging
 logging.basicConfig(level=logging.INFO,format='%(asctime)s %(filename)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
@@ -14,11 +17,10 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app=web.Application(loop=loop)
     app.router.add_route('GET','/',index)
-    serve=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    serve=await loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info('server started at http://127.0.0.1:9000')
     return serve
 
