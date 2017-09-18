32e6f9f098521b817b20a39ff6b531cea21cf214
xiaoyuann <602632139@qq.com>
2017-09-18 23:53:12 +0800

    1

--- webapp/www/orm.py
+++ webapp/www/orm.py
@@ -26,3 +26,41 @@ async def create_pool(loop,**kw):
         loop=loop
     )
 
+async def select(sql,args,size=None):
+    log(sql,args)
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.curor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?','%s'),args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+            logging.info('rows returned: %s ' % len(rs))
+            return rs
+
+async def execute(sql,args,autocommit=True):
+    log(sql)
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?','%s'.args))
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
+
+def create_args_string(num):
+    L=[]
+    for i in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+class Model(dict,metaclass=ModelMetaclass):
+    
