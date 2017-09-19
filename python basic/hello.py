0e813c92df78591b77e6fc8646b0d78f4c5cc5bb
xiaoyuann <602632139@qq.com>
2017-09-19 22:03:41 +0800

    1

--- python basic/hello.py
+++ python basic/hello.py
@@ -313,7 +313,7 @@ def getHTML(url):
         return '出现异常'
 
 url='http://www.sina.com'
-print(getHTML(url))'''
+print(getHTML(url))
 
 import  requests
 
@@ -326,5 +326,29 @@ try:
     print(r.text[:5000])
     print(r.request.headers)
 except:
+    print('爬取失败')'''
+
+import requests
+import os
+url = ''
+root = 'E://mov//'
+path = root+url.split('/')[-1]
+kv = {'user-agent':'Mozilla/5.0'}
+try:
+    if not os.path.exists(root):
+        os.mkdir(root)
+    if not os.path.exists(path):
+        r = requests.get(url,headers=kv)
+        with open(path,'wb') as f:
+            f.write(r.content)
+            f.close()
+            print('保存成功')
+    else:
+        print("文件已存在")
+except:
     print('爬取失败')
+
+
+
+
 
