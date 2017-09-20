080c634d1b1b857a46c975eec29a394c45eea051
xiaoyuann <602632139@qq.com>
2017-09-21 00:01:19 +0800

    1

--- python basic/spider1.py
+++ python basic/spider1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+#爬取淘宝手机信息
 
 __author__ = 'Aurora'
 
@@ -8,9 +9,9 @@ import re
 
 def getHTMLText(url):
     try:
-        kv = {'user-agent':'Mozilla/5.0'}
+        kv = {'user-agent':'Mozilla/5.0'}#模拟浏览器发起访问
         r = requests.get(url,timeout=30,headers=kv)
-        r.raise_for_status()
+        r.raise_for_status()#检测是否成功响应服务器
         r.encoding = r.apparent_encoding
         return r.text
     except:
@@ -18,7 +19,7 @@ def getHTMLText(url):
     
 def parsePage(infolist,html):
     try:
-        plt = re.findall(r'\"price\"\:\"[\d\.]+\"',html)
+        plt = re.findall(r'\"price\"\:\"[\d\.]+\"',html)#使用正则匹配所需信息
         tlt = re.findall(r'\"title\"\:\".*?\"',html)
         for i in range(len(tlt)):
             price = eval(plt[i].split(':')[1]) + '元'
@@ -28,7 +29,7 @@ def parsePage(infolist,html):
         return ''
 
 def printGoodsList(goodsInfo):
-    ptlt = '{:3}\t{:10}\t{:10}'
+    ptlt = '{:3}\t{:10}\t{:10}'#格式化输出信息
     print(ptlt.format('序号','价格','商品名称'))
     count = 0
     for info in goodsInfo:
@@ -38,7 +39,7 @@ def printGoodsList(goodsInfo):
 
 def main():
     goods = '手机'
-    pages = 10
+    pages = 10#访问网页数
     init_url = 'https://s.taobao.com/search?q=' + goods
     goodsInfo = []
     for i in range(pages):
