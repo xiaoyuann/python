0559512c53977587fda3386e01dfe7159549f89c
xiaoyuann <602632139@qq.com>
2017-09-22 13:30:06 +0800

    1

--- python basic/loggings.py
+++ python basic/loggings.py
@@ -7,19 +7,27 @@ import  requests
 from bs4 import BeautifulSoup
 
 LOGIN_URL = 'http://idas.uestc.edu.cn/authserver/login'
-get_url = 'http://eams.uestc.edu.cn/eams/home!childmenus.action?menu.id=844'
+get_url = 'http://eams.uestc.edu.cn/eams/courseTableForStd.action?_=1506058028934'
+
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36'
 headers = {'User-Agent':user_agent,'Connection':'keep-alive'}
+
 uestc_session = requests.Session()
 f = uestc_session.get(LOGIN_URL,headers=headers)
-soup = BeautifulSoup(f.content,'html.parser')
+soup = BeautifulSoup(f.text,'html.parser')
 lt = soup.find('input',{'name':'lt'})['value']
-print(lt)
+execution = soup.find('input',{'name':'execution'})['value']
+
 values = {
     'username':'2016060101007',
     'password':'',
-    'lt':lt
+    'lt':lt,
+    'execution':execution,
+    '_eventId':'submit',
+    'rmShown':'1',
+    'dllt':'userNamePasswordLogin'
     }
-uestc_session.post(url,data=values,headers=headers)
+
+uestc_session.post(LOGIN_URL,data=values,headers=headers)
 f = uestc_session.get(get_url,headers=headers)
-print(f.content.decode('utf-8'))
+print(f.text)
