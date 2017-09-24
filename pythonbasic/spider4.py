fca79dcf6dcbd7a69f1fde023272934a05c965b6
xiaoyuann <602632139@qq.com>
2017-09-25 00:24:59 +0800

    14

--- pythonbasic/spider4.py
+++ pythonbasic/spider4.py
@@ -3,12 +3,23 @@
 
 __author__ = 'Aurora-Twinkle'
 
-import urllib.request
+import urllib.request,urllib.error
+from bs4 import BeautifulSoup
 
 url = 'http://www.qiushibaike.com/hot/page/1'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36'
 headers = {'User-Agent':user_agent,'Connection':'keep-alive'}
-
-request = urllib.request.Request(url,headers=headers)
-response = urllib.request.urlopen(request)
-print(response.read().decode())
+try:
+    request = urllib.request.Request(url,headers=headers)
+    response = urllib.request.urlopen(request)
+    content = response.read().decode('utf-8')
+    soup = BeautifulSoup(content,'html.parser')
+    for s in soup.findAll('h2'):
+    	#HAVEimg = re.search('img',item[3])
+    	#if not HAVEimg:
+        print(s.string)
+except urllib.error.URLError as e:
+	if hasattr(e,'code'):
+		print(e.code)
+	if hasattr(e,'reason'):
+		print(e.reason)
