00038c2da01a62c8b0a30ab1f0b6cc336619d0d2
xiaoyuann <602632139@qq.com>
2018-03-24 21:50:46 +0800

    1

---
+++ leetcode/Rotate Array.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author__ = 'Aurora-Twinkle'
+
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
