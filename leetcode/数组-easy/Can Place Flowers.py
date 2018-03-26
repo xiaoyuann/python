4fe7f274ab903a1682897dfcd42fc55fe1da30dc
xiaoyuann <602632139@qq.com>
2018-03-26 18:35:13 +0800

    1

---
+++ leetcode/数组-easy/Can Place Flowers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author__ = 'Aurora-Twinkle'
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        
