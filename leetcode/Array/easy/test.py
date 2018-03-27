13a971dcd12d7e17ffa96f02e0ba8c5453f0a4de
xiaoyuann <602632139@qq.com>
2018-03-27 19:36:02 +0800

    完成数组简单难度

---
+++ leetcode/Array/easy/test.py
@@ -0,0 +1,10 @@
+def moveZeroes():
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        
+       
+
+l=[1,2,3,4,5,6]
+print(l[:-1])
