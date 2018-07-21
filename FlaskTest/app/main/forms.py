958da61d13c99f1602577d375286aa58340ba00e
xiaoyuann <602632139@qq.com>
2018-07-22 01:00:31 +0800

    1

--- FlaskTest/app/main/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
-from wtforms.validators import Required
-
-class CommentForm(FlaskForm):
-    message = TextAreaField(validators=[Required()])
-    submit = SubmitField('Comment')
--- pythonbasic/spider3.py
+++ pythonbasic/spider3.py
@@ -273,3 +273,4 @@ class Taobao:
  
 taobao = Taobao()
 taobao.main()
+
